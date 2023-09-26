6a72623d550f6f63f36e850cf2455c59cbb3383b
alyssa0lsen <aols711@aucklanduni.ac.nz>
2023-09-27 08:12:14 +1300

    Plot for domain convergence of height

--- AerodynamicDomainGraphs.py
+++ AerodynamicDomainGraphs.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 # Read the CSV data into a DataFrame
 domainRightDF = pd.read_csv('DomainRight.csv')
 domainLeftDF = pd.read_csv('DomainLeft.csv')
+domainVerticalDF = pd.read_csv('vertical 3.csv')
 
 # DOMAIN SIZE PLOTS
 # INLET TO AIRFOIL
@@ -42,3 +43,24 @@ plt.title('Domain convergence - outlet to airfoil')
 plt.show()  # Display the graph
 
 # VERTICAL
+vertical = domainVerticalDF['P15 - Vertical [m]']
+dragCoefficient = domainVerticalDF['P14 - drag-coefficient-op']
+
+marker_styles = ['.'] * len(vertical)
+
+# Set a specific marker style for a data point (e.g., 's' for a star) - for the 3rd point (index 2)
+highlight_index = 2
+marker_styles[highlight_index] = '*'
+
+plt.plot(vertical, dragCoefficient, linestyle='-', color='#1b96c6', label='Data Line')
+
+# Plot the data with customized marker styles
+for x, y, marker in zip(vertical, dragCoefficient, marker_styles):
+    plt.plot(x, y, marker=marker, linestyle='-', color='#1b96c6', label='Data Points')
+
+#plt.plot(inletToAirfoil, dragCoefficient,marker='o', label='Cd', color='#1b96c6')
+
+plt.xlabel('Height of Domain (m)')
+plt.ylabel('Cd')
+plt.title('Domain convergence - height')
+plt.show()  # Display the graph
